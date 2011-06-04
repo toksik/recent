2f445c87e11db19d6dd1d75c40a0fc53406e007e
Julian Rother <julian@toksik.org>
2011-06-05 01:21:58 +0200

    Add dependency class for X11

--- lib/deps/__init__.py
+++ lib/deps/__init__.py
@@ -2,4 +2,7 @@ def get_class(id):
     if id == 'inet':
         import lib.deps.inet
         return lib.deps.inet.InetDep
+    elif id == 'x11':
+        import lib.deps.x11
+        return lib.deps.x11.X11Dep
     return None
---
+++ lib/deps/x11.py
@@ -0,0 +1,13 @@
+import subprocess
+
+from lib.dep.base import Dependency
+
+class X11Dep(Dependency):
+    id = 'x11'
+    def check(self):
+        p = subprocess.Popen(['ps','-A'], stdout=subprocess.PIPE)
+        p.wait()
+        p_list = p.stdout.read()
+        if b'X' in p_list:
+            return True
+        return False
