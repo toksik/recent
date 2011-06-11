e04f25ad2d4a030994e78b93605000dd2fc55c55
Julian Rother <julian@toksik.org>
2011-06-11 10:35:15 +0200

    Add provider module for statusnet

--- lib/providers/__init__.py
+++ lib/providers/__init__.py
@@ -2,4 +2,7 @@ def get_class(id):
     if id == 'feed':
         import lib.providers.feed
         return lib.providers.feed.FeedProvider
+    elif id == 'statusnet':
+        import lib.providers.statusnet
+        return lib.providers.statusnet.StatusnetProvider
     return None
---
+++ lib/providers/statusnet.py
@@ -0,0 +1,55 @@
+import lib.providers.base
+import lib.markup
+from lib.manager import Notification
+
+import feedparser
+
+class StatusnetProvider(lib.providers.base.Provider):
+    name = 'Statusnet'
+    deps = ['inet']
+    config_keys = ['api', 'user', 'deny_tags']
+    interval = 60
+    def is_valid(self, tags):
+        if self.config['deny_tags']:
+            denied = [s.strip() for s in self.config['allow_tags'].split(',')]
+            for tag in tags:
+                if tag in denied:
+                    return False
+        return True
+                
+    def update(self):
+        feed = feedparser.parse(self.config['api']+\
+                '/statuses/friends_timeline/%s.atom'%self.config['user'])
+        new_entries = []
+        for entry in feed.entries:
+            id = ''
+            if 'id' in entry:
+                id = entry.id
+            author = ''
+            if 'author' in entry:
+                author = entry.author
+            body = ''
+            if 'content' in entry and entry.content and \
+                   'value' in entry.content[0]:
+                if entry.content[0].type == 'text/html':
+                    body = lib.markup.html_to_log(entry.content[0].value,
+                                                  reformat=False,
+                                                  newlines=False)
+                else:
+                    body = entry.content[0].value
+            link = ''
+            if 'link' in entry:
+                link = entry.link
+            if 'tags' in entry:
+                tags = []
+                for tag in entry.tags:
+                    if 'term' in tag:
+                        tags.append(tag.term)
+                if not self.is_valid(tags):
+                    continue
+            if not self.manager.state.check(self.id, id):
+                new_entries.append(Notification(provider=self.name, id=id,
+                                        title=body, author=author, link=id))
+        for entry in new_entries[::-1]:
+            self.manager.state.add(self.id, entry.id)
+            self.manager.notify(entry)
